67db60bcffc77859f318ad29dae79709f9c01a4a
aoffyzuzaz <aoffyzuzaz@gmail.com>
2014-06-26 00:01:17 +0700

    ajax edit course name and course description

--- sLC/src/main/assets/www/js/teacher/script_tea_get_data.js
+++ sLC/src/main/assets/www/js/teacher/script_tea_get_data.js
@@ -28,7 +28,7 @@ function queryUserDB(tx) {
 // Query the success callback
 function querySuccessUser(tx, results) {
 	var db = window.openDatabase(database_name,database_version, database_displayname, database_size);										
-		db.transaction(queryCourseDB, errorCB);	
+	db.transaction(queryCourseDB, errorCB);	
 }
 
 // Query the database
@@ -41,6 +41,7 @@ function querySuccess(tx, results) {
 	var len = results.rows.length;
 	edit_cname = results.rows.item(0).cname;
 	edit_cdes = results.rows.item(0).cdescription;
+	$(".hidden_ccode").val(results.rows.item(0).ccode);
 	$("#edit_cname").html(edit_cname);
 	$("#course-name-input").val(edit_cname);
 	$("#tea_cou_des").html(edit_cdes);
@@ -48,7 +49,109 @@ function querySuccess(tx, results) {
 	//alert(results.rows.item(0).ccode);
 }
 
+function execUpdateCname(tx) {
+	console.log("UPDATE "+course_db+" SET cname = '"+edit_cname+"' WHERE ccode = '"+send_coursecode+"'");
+	tx.executeSql('UPDATE '+course_db+' SET cname = "'+edit_cname+'" WHERE ccode = "'+send_coursecode+'"');
+	$("#edit_cname").html(edit_cname);
+	$("#course-name-input").val(edit_cname);
+	$("#tea_cou_name-1").css("display","block");
+    $("#tea_cou_name-2").css("display","none");	
+}
+
+function execUpdateCdes(tx) {
+	console.log("UPDATE "+course_db+" SET cdescription = '"+edit_cdes+"' WHERE ccode = '"+send_coursecode+"'");
+	tx.executeSql('UPDATE '+course_db+' SET cdescription = "'+edit_cdes+'" WHERE ccode = "'+send_coursecode+'"');
+	$("#tea_cou_des").html(edit_cdes);
+	$("#course-description-textarea").html(edit_cdes);
+	$("#tea_cou_con_coll_CouDes-1").css("display","block");
+   $("#tea_cou_con_coll_CouDes-2").css("display","none");
+}
+
 //jQuery    
 $( document ).ready(function() {
-	
+	//Edit Coursename Form
+	$("#edit_cname_btn_submit").click(function() {
+		//Callback handler for form submit event
+			$("#edit_cname_form").submit(function(e)
+			{			 
+				var formObj = $(this);
+				var formURL = formObj.attr("action");
+				var formData = new FormData(this);
+				$.ajax({
+					url: formURL,
+					type: 'POST',
+					data:  formData,
+					mimeType:"multipart/form-data",
+					dataType : "json",
+					contentType: false,
+					cache: false,
+					processData:false,
+					async: false,
+					success: function(data, textStatus, jqXHR){
+						if(data.status == "OK") {
+							edit_cname = data.cname;
+							send_coursecode = data.ccode;		
+							toast(data.message);						
+                			var db = window.openDatabase(database_name,database_version, database_displayname, database_size);										
+							db.transaction(execUpdateCname, errorCB);												
+						} else {
+							//alert(data.message);
+							toast(data.message);
+						}							
+					}, //end success
+					error: function(jqXHR, textStatus, errorThrown) {
+						//alert("ERROR");
+						alert(jqXHR.responseText);
+						//alert(thrownError);
+					} //end error         
+				});
+				e.preventDefault(); //Prevent Default action. 
+				e.unbind();
+			}); 
+   });  
+   //end of Edit Coursename Form
+   
+   //Edit Course Description Form
+	$("#edit_cdes_btn_submit").click(function() {
+		//Callback handler for form submit event
+			$("#edit_cdes_form").submit(function(e)
+			{			 
+				var formObj = $(this);
+				var formURL = formObj.attr("action");
+				var formData = new FormData(this);
+				$.ajax({
+					url: formURL,
+					type: 'POST',
+					data:  formData,
+					mimeType:"multipart/form-data",
+					dataType : "json",
+					contentType: false,
+					cache: false,
+					processData:false,
+					async: false,
+					success: function(data, textStatus, jqXHR){
+						if(data.status == "OK") {
+							edit_cdes = data.cdes;
+							send_coursecode = data.ccode;		
+							toast(data.message);						
+                			var db = window.openDatabase(database_name,database_version, database_displayname, database_size);										
+							db.transaction(execUpdateCdes, errorCB);												
+						} else {
+							//alert(data.message);
+							toast(data.message);
+						}							
+					}, //end success
+					error: function(jqXHR, textStatus, errorThrown) {
+						//alert("ERROR");
+						alert(jqXHR.responseText);
+						//alert(thrownError);
+					} //end error         
+				});
+				e.preventDefault(); //Prevent Default action. 
+				e.unbind();
+			}); 
+   });  
+   //end of Edit Course Description Form
+   
 });
+
