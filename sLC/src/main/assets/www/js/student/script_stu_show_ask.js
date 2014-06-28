e935a1548276bf82ba21b9e8502cd7eac34ca172
aoffyzuzaz <aoffyzuzaz@gmail.com>
2014-06-29 06:11:08 +0700

    AJAX : show ask list view (student)

---
+++ sLC/src/main/assets/www/js/student/script_stu_show_ask.js
@@ -0,0 +1,30 @@
+//jQuery    
+$( document ).ready(function() {		
+	$(".header_title").html(stu_cname);	
+	var chk_connect = checkConnection();
+	if(chk_connect != "no") {
+		$.ajax({
+					url: "http://service.oppakub.me/SLC/chk_stu_course_lesson.php",
+					type: 'POST',
+					data:  "cid="+stu_cid,
+					dataType : "json",
+					async: false,
+					success: function(data, textStatus, jqXHR){
+					if(data.status == "OK") {
+						var data_len = data.data.length;					
+						for(var i =0;i<data_len;i++) {
+								$("#stu_list_ask").append('<li><a href="student_course_ask_lesson.html" data-transition="none" name="'+data.data[i].lid+'">'+data.data[i].lname+' </a></li>'+"\n").trigger('create');     				
+						}	
+										
+					} else {
+						toast(data.message);					
+					}								
+				}, //end success
+					error: function(jqXHR, textStatus, errorThrown) {
+						alert(jqXHR.responseText);
+					} //end error         
+		});
+	} else {
+		toast('Please connect to the internet.');
+	}
+});
