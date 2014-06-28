73336f9820c329942ccef5086ad36e4a8a75de05
aoffyzuzaz <aoffyzuzaz@gmail.com>
2014-06-29 05:58:46 +0700

    AJAX : show lesson list view (student)

--- sLC/src/main/assets/www/js/student/script_stu_show_lesson.js
+++ sLC/src/main/assets/www/js/student/script_stu_show_lesson.js
@@ -1,11 +1,10 @@
 //jQuery    
-$( document ).ready(function() {	
-	$(".header_title").html(stu_cname);
-	
+$( document ).ready(function() {		
+	$(".header_title").html(stu_cname);	
 	var chk_connect = checkConnection();
 	if(chk_connect != "no") {
 		$.ajax({
-					url: "http://service.oppakub.me/SLC/chk_stu_course_book.php",
+					url: "http://service.oppakub.me/SLC/chk_stu_course_lesson.php",
 					type: 'POST',
 					data:  "cid="+stu_cid,
 					dataType : "json",
@@ -14,7 +13,7 @@ $( document ).ready(function() {
 					if(data.status == "OK") {
 						var data_len = data.data.length;					
 						for(var i =0;i<data_len;i++) {
-								$("#student_course_book_list").append('<li><a href="#" onclick="window.open(\''+data.data[i].blink+'\', \'_system\');" data-transition="none" name="'+data.data[i].bid+'"><img src="../img/book-'+data.data[i].btype+'.png"><h3>'+data.data[i].bname+'</h3><p>Added : '+data.data[i].bdate_created+'</p> </a></li>'+"\n").trigger('create');     				
+								$("#stu_list_lesson").append('<li><a href="student_course_lesson_content.html" data-transition="none" name="'+data.data[i].lid+'">'+data.data[i].lname+' </a></li>'+"\n").trigger('create');     				
 						}	
 										
 					} else {
