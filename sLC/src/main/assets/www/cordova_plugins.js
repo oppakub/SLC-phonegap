c76f4b70f84176581f8e4a45f3d26a5a1edff1c4
aoffyzuzaz <aoffyzuzaz@gmail.com>
2014-07-28 21:21:15 +0700

    add facebook feature

--- sLC/src/main/assets/www/cordova_plugins.js
+++ sLC/src/main/assets/www/cordova_plugins.js
@@ -321,6 +321,13 @@ module.exports = [
         "clobbers": [
             "window.open"
         ]
+    },
+    {
+        "file": "plugins/com.phonegap.plugins.facebookconnect/www/phonegap/plugin/facebookConnectPlugin/facebookConnectPlugin.js",
+        "id": "com.phonegap.plugins.facebookconnect.FacebookConnectPlugin",
+        "clobbers": [
+            "window.facebookConnectPlugin"
+        ]
     }
 ];
 module.exports.metadata = 
@@ -337,7 +344,8 @@ module.exports.metadata =
     "org.apache.cordova.dialogs": "0.2.9-dev",
     "org.apache.cordova.vibration": "0.3.10-dev",
     "org.apache.cordova.globalization": "0.2.9-dev",
-    "org.apache.cordova.inappbrowser": "0.5.1-dev"
+    "org.apache.cordova.inappbrowser": "0.5.1-dev",
+    "com.phonegap.plugins.facebookconnect": "0.7.0"
 }
 // BOTTOM OF METADATA
 });
